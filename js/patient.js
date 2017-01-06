1aa628f0f371d996ec9bd3461e8fad48234f1d51
Adam Zinder <adam.zinder@gmail.com>
2017-01-06 14:46:11 -0800

    added condtion if api returns an empty array

--- js/patient.js
+++ js/patient.js
@@ -11,12 +11,18 @@ Patient.prototype.getDoctors = function(displayFunction) {
   medicalIssue = this.medicalIssue;
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
-     displayFunction(result);
+     console.log(result)
+     console.log(result.length);
+     if (result.data.length > 0) {
+       displayFunction(result);
+     } else {
+       $('#results').show();
+       $('#doctors').append("Sorry we couldn't find a doctor under your search criteria, please try again");
+     }
     })
    .fail(function(error){
-     console.log("hello");
-      $('#results').show();
-      $('#doctors').append("Sorry we couldn't find a doctor under your search criteria, please try again");
+     $('#results').show();
+     $('#doctors').append("Sorry we couldn't find a doctor under your search criteria, please try again");
     });
 };
 
